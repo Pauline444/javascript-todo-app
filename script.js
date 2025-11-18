c45cd4750a44a9dd549922143a6ee075409fcedd
Pauline Pettersson <pauline-pettersson@hotmail.com>
2025-11-18 15:46:11 +0100

    add input to array

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const ul = document.getElementById('todo-list');
 
-let allTodos = getTodos();
-updateTodoList();
+let allTodos = [];
 
 todoForm.addEventListener('submit', event => {
     event.preventDefault();
     addTodo();
 });
 
+// trim tar bort oönskade mellanslag
+function addTodo() {
+    const todoText = todoInput.value.trim();
+    if (todoText.length > 0) {
+        allTodos.push(todoText);
+        console.log(allTodos);
+        todoInput.value = '';
+    }
+};
