d7861b9ec12152e1b1c39e1e95f17f21298d52b1
Pauline Pettersson <pauline-pettersson@hotmail.com>
2025-11-18 15:40:18 +0100

    selecting elements and add eventlistener to form

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const todoForm = document.querySelector('form');
+const todoInput = document.getElementById('todo-input');
+const ul = document.getElementById('todo-list');
+
+let allTodos = getTodos();
+updateTodoList();
+
+todoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    addTodo();
+});
+
