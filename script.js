c302ef37fd41d3b073eb7ede8f88d14c88f0d193
Pauline Pettersson <pauline-pettersson@hotmail.com>
2025-11-18 15:51:02 +0100

    create list todo items and update the list

--- script.js
+++ script.js
@@ -16,7 +16,35 @@ function addTodo() {
     const todoText = todoInput.value.trim();
     if (todoText.length > 0) {
         allTodos.push(todoText);
-        console.log(allTodos);
+        updateTodoList();
         todoInput.value = '';
     }
+};
+
+function updateTodoList() {
+    ul.innerHTML = '';
+    allTodos.forEach((todo, todoIndex) => {
+        const todoItem = createTodoItem(todo, todoIndex);
+        ul.append(todoItem);
+    })
+}
+
+function createTodoItem(todo, todoIndex) {
+    const todoId = 'todo-' + todoIndex;
+    const li = document.createElement('li');
+    li.className = 'todo';
+    li.innerHTML = `
+    <input type="checkbox" id="${todoId}">
+                <label for="${todoId}" class="custom-checkbox">
+                    <img src="sources/check_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png">
+                </label>
+                <label for="${todoId}" class="todo-text">
+                    ${todo}
+                </label>
+                <button class="delete-btn">
+                    <img src="sources/close_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png">
+                </button>
+    `;
+
+    return li;
 };
